810648954fd6940851ef3f068338e230eb993413
sevvalozdemir <115032656+sevvalozdemir@users.noreply.github.com>
2022-12-21 14:21:36 +0300

    Add files via upload

---
+++ sorularvecevaplar.h
@@ -0,0 +1,12 @@
+struct soru soruvecevaplar[10] = {
+    { "1. Soru: Ýlk olarak gün içerisinde yaptýðýn rutin iþlerini yaparken endiþe duyuyor musun? :", 1 ,3},
+    { "2. Soru: Peki kalabalýk bir ortama girerken bütün gözler sanki senin üstündeymiþ gibi hissediyor musun? :", 1 ,5},
+    { "3. Soru: Kendini son zamanlarda ne yaparsan yap mutsuz hissediyor musun? :", 1 ,8},
+    { "4. Soru: Dýþarý çýktýðýnda insanlarla muhabbet ederken anda kalmak yerine kafanda baþka þeyler düþünüyor musun? :", 1 ,5},
+    { "5. Soru: Herhangi bir þey düþünürken durduk yere kalp çarpýntýsý yaþadýðýn oluyor mu? :", 1 ,10},
+    { "6. Soru: Geceleri sürekli bir þeyler düþünüp uyuyamadýðýn oluyor mu? :", 1 ,5},
+    { "7. Soru: Her ne olursa olsun her zaman en kötü senaryoyu mu düþünürsün? :", 1 ,12},
+    { "8. Soru: Son zamanlarda yapman gereken bir iþe koyulmak için kendinde enerji bulamadýðýn oluyor mu? :", 1 ,5},
+    { "9. Soru: Hayatýnda veya hayatýnda olmayan insanlarý hep bir tehlike olarak gördüðün oluyor mu? :", 1 ,7},
+    { "10. Soru: Son olarak, insanlara alay konusu olacaðým ya da rezil olacaðým diye içinden geleni yapmadýðýn þeyler oluyor mu? :", 1 ,5},
+};
