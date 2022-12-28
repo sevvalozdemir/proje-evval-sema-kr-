af77e2049e3e859a8f5715e6bc022c8bf987d36c
sevvalozdemir <115032656+sevvalozdemir@users.noreply.github.com>
2022-12-28 11:26:45 +0300

    Add files via upload

--- projemain.c
+++ projemain.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include<locale.h>
-#include "fonksiyonlar.h"
 #include "sorularvecevaplar.h"
+#include "fonksiyonlar.h"
 #include <unistd.h>
-
-
 int main()
 {
 	setlocale(LC_ALL, "Turkish");
     int inputanswers[10];
     int puan;
-    printf("*********************Bu 10 Soruluk Evet/Hayýr Testine Göre Ne Kadar Anksiyetiksin?*********************\n\n\n");
-    printf("Bu testimize göre size ne kadar anksiyetik olduðunuzu söylüyoruz?\nHazýrsanýz;\nHaydi Teste!!\n");
-    printf("NOT: Bu bir eðlence testidir.Konuyla ilgili iþin uzmanýna danýþmanýzý tavsiye ederiz:))\n\n\n");
-    printf("Aþaðidaki sorulari evet(1) veya hayir(0) olarak cevaplandiriniz.\n\n");
+    printf("***Bu 10 Soruluk Evet/Hayýr Testine Göre Ne Kadar Anksiyetiksin?***\n\n\nBu testimize göre size ne kadar anksiyetik olduðunuzu söylüyoruz?\nHazýrsanýz;\nHaydi Teste!!\nNOT: Bu bir eðlence testidir.Konuyla ilgili iþin uzmanýna danýþmanýzý tavsiye ederiz:))\n\n\nAþaðidaki sorulari evet(1) veya hayir(0) olarak cevaplandiriniz.\n\n");
     int i;
-    for ( i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++) {
         printf("%s",soruvecevaplar[i].soru);
         scanf("%d",&inputanswers[i]);
     }
